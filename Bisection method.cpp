2b4c95145ea710b163e83712f9a03b2e34001c30
Arnav Sharma <79043654+Arnav-Sharmaa@users.noreply.github.com>
2021-09-08 23:11:21 +0530

    Add files via upload

---
+++ Bisection method.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+using namespace std;
+#include <math.h>
+#include <cstdlib>
+
+// Bisection method for numerical solution of non Linear equations //
+double f(double x)
+{
+    return x*x*x - 5*x +1 ;
+}
+void bisection(double a,double b)
+{
+    double m;
+    if (f(a)*f(b)>0)
+    {
+        cout<<"Invalid values for a and b .";
+        return;
+    }
+    else
+    {
+        if (z==0)
+        {
+            do
+            {
+                m=(a+b)/2;
+                cout<<m<<endl;
+                if (f(m)==0)
+                {
+                    break;
+                }
+                else if (f(a)*f(m)<0)
+                {
+                    b=m;
+                }
+                else
+                {
+                    a=m;
+                }
+            }while (abs(b-a)>=error));
+
+        }
+        else if(z==1)
+        {
+            for(int i=0;i<n;i++)
+            {
+                m=(a+b)/2;
+                cout<<m<<endl;
+                if (f(m)==0)
+                {
+                    break;
+                }
+                else if (f(a)*f(m)<0)
+                {
+                    return bisection(a,m);
+                }
+                else
+                {
+                    return bisection(m,b);
+                }           
+            }
+        }
+        else
+        {
+            cout<<"invalid"<<endl;
+        }
+
+    }
+
+}
+int main()
+{
+    double a,b,error;
+    int  z,n;
+    cout<<"Enter a value for a : ";
+    cin>>a;
+    cout<<"Enter a value for b : ";
+    cin>>b;
+    cout<<"Enter 0 to specify Maximum error || Enter 1 to specify maximum number of iterations : ";
+    cin>>z;
+    if (z==0)
+    {
+        cout<<"Enter the max error : ";
+        cin>>error;
+    }
+    else if(z==1)
+    {
+        cout<<"Enter the number of iterations : ";
+        cin>>n;
+    }
+    else
+    {
+        cout<<"Invalid input";
+    }
+    bisection(a,b);
+
+}
